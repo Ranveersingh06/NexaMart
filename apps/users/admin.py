064e948f27c937605ef16a12f20110117bab1d83
Ranveersingh06 <ranveersingh0699@gmail.com>
2026-03-31 12:35:14 +0530

    Improve user admin configuration

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+# Admin configuration updated
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ['email', 'first_name', 'last_name', 'role', 'is_active']
