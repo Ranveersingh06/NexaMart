7462264070b7230020e2917aaf363572d63ee8b9
Ranveersingh06 <ranveersingh0699@gmail.com>
2026-03-31 12:33:10 +0530

    Register payment models in Django admin

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'order', 'payment_method', 'payment_status', 'amount']
+    list_filter = ['payment_status', 'payment_method']
+    search_fields = ['customer__email', 'transaction_id']
+    readonly_fields = ['transaction_id', 'created_at', 'updated_at']
